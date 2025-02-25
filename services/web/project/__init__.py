41bb4097d25bcaf9cf27c10e12b1215ff25e368a
buffeinstein <ambikatiwari999@gmail.com>
2025-02-24 20:42:07 -0800

    up till static files

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
@@ -21,3 +20,8 @@ class User(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
